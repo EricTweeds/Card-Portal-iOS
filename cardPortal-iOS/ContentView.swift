a346a7d4345066db5a394c022ed08f60ddebc531
Eric Tweedle <erictweeds@gmail.com>
2020-11-24 23:20:07 -0500

    Initial Commit

---
+++ cardPortal-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cardPortal-iOS
+//
+//  Created by Eric Tweedle on 2020-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cardPortal-iOS/cardPortal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardPortal_iOSApp.swift
+//  cardPortal-iOS
+//
+//  Created by Eric Tweedle on 2020-11-24.
+//
+
+import SwiftUI
+
+@main
+struct cardPortal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
